b18a64c67ede35ac4d9341f0f1ceb42a42049f97
premchand13-gif <premechand1301@gmail.com>
2023-01-14 11:06:06 +0530

    Modification 3

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -7,12 +7,12 @@ public class Main {
 //        System.out.println(obj.fun());
     }
     public static class A{
-        String meth(){
+        public String meth(){
             return "Invoking method from class A";
         }
     }
     public static class B extends A {
-        String meth(){
+        public String meth(){
 
             return "Method is overridden in Extendend class B";
         }
